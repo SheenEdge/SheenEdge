4f106dd96045b4dd5472f0fb808f0e55d3d48d88
SheenEdge <sheenedgehelp@gmail.com>
2024-10-13 11:45:39 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,7 @@ const port =process.env.PORT || 5000;
 app.use(express.json());
 app.use(errorHandler) 
 app.use(cors({
-	origin: 'http://localhost:5173', 
+	origin: 'https://sheenedge-frontend.vercel.app/', 
 	credentials: true
   }));
 
