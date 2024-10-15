9db6948c338d3454215d2dedd0125ee992fdc2f7
KushalSharmaGit <kushalsharma8762@gmail.com>
2024-10-15 14:20:27 +0530

    Production Changes

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,7 @@ const port =process.env.PORT || 5000;
 app.use(express.json());
 app.use(errorHandler) 
 app.use(cors({
-	origin: 'https://www.sheenedge.com',
+	origin: process.env.BASE_URL,
     	credentials: true,
     	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     	allowedHeaders: ['Content-Type', 'Authorization'],
